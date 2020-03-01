b967e8ed6e3641b5c5aba9d40643ce5e2ca88369
Sabbir Ahmed <45328851+sabbir030@users.noreply.github.com>
2020-03-01 17:40:55 +0600

    test commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Loader Animation
+/* $(window).on("load", function() {
+  let loading = document.querySelector(".loading");
+  let letters = loading.textContent.split("");
+  loading.textContent = "";
+  letters.forEach((letter, i) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.style.animationDelay = `${i / 5}s`;
+    loading.append(span);
+  });
+
+  $(".loading")
+    .fadeIn(3000)
+    .delay(3500)
+    .fadeOut("slow");
+});
+ */
+
+//init WOW
+new WOW().init();
+
+//Timeout
+
+setTimeout(function() {
+  document.getElementById("hi-tech").style.display = "block";
+  console.log("wilton hi-tech park");
+}, 5000);
+
+setTimeout(function() {
+  document.getElementById("visit-btn").style.display = "block";
+  console.log("Visit Website");
+}, 7000);
